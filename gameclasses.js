f58e71210c94d281b3910d5fb1fd80cd09ee9186
moritz.gg <moritat0511@gmail.com>
2025-12-15 01:15:24 +0200

    x

--- gameclasses.js
+++ gameclasses.js
@@ -1,4 +1,3 @@
-// import { Canvas, TextBlock, TextureBlock, ColorBlock, ButtonQuiet } from './ui.js';
 import { onDisconnect, ref, onValue, set, push , get, remove } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js";
 import { db } from './firebasedata.js';
 import { log } from './utils.js';
@@ -9,8 +8,6 @@ class game {
     constructor() {
         this.id = "error";
         this.info = {};
-        // this.sessions = [];
-        // this.Init()
     }
 
     async init() {
@@ -21,30 +18,9 @@ class game {
         }
     }
 
-    // setSession(index, data) {
-    //     set(ref(db, `games/${this.id}/sessions/${index}`), data);
-    //     this.sessions[index] = data
-    //     if (data.endsession) {
-    //         location.href = "../";
-    //         this.end();
-    //         console.log(`end session ${index}`)
-    //     }
-    // }
-
-    // addSession(data) {
-    //     const newindex = this.sessions.length
-    //     set(ref(db, `games/${this.id}/sessions/${newindex}`), data);
-    //     this.sessions[newindex] = data
-    //     onValue(ref(db, `games/${this.id}/sessions/${newindex}`), snap => {
-    //         if (snap.exists() && Object.keys(snap.val().players).length == this.playerAmount) {
-    //             document.getElementsByClassName("loading").style.display = "none";
-    //         }
-    //         console.log(`new data for ${this.id} session ${newindex}`)
-    //     });
-    // }
-
-    async start() {
-        console.log(`${this.id} started`)
+    async start(sessionid) {
+        log(`${this.id} started`)
+        set(ref(db, `games/${this.id}/sessions/${sessionid}/started`), true)
     }
 
     async addPlayer(ip) {
@@ -90,9 +66,9 @@ class game {
         const playerRef = ref(db, `games/${this.id}/sessions/${targetSessionId}/players/${index}`);
         await set(playerRef, ip);
 
-        onDisconnect(playerRef).remove();
+        if (index +1 == this.info.playerAmount) this.start(targetSessionId)
 
-        return { sessionId: targetSessionId, playerIndex: index };
+        onDisconnect(playerRef).remove();
     }
 
 
@@ -105,106 +81,9 @@ class game {
 }
 
 export class Tik extends game {
-//     constructor() {
-//         super();
-//         this.playerAmount = 2;
-//         this.name = "TikTakToe";
-//         this.imagedata = {image: "Assets/tiktaktoe.png", size: 30};
-//         this.gameUI = { Can: new Canvas(), blocks: Array.from({ length: 9 }, () => ({
-//             image: new TextureBlock("Assets/load.png", 120, 0),
-//             button: new ButtonQuiet("", 120, 120, 0, 0, 40, "#00000000", false)
-//         })) };
-//     }
-
-//     load(i) {
-//         return super.load(i);
-//     }
-
-//     async start() {
-//         const path = ref(db, `games/${this.name}`);
-//         const snap = await get(path);
-
-//         // Spielfeld initialisieren
-//         if (!snap.exists()) {
-//             await set(path, {
-//                 board: "000000000",
-//                 turn: "X",     // X beginnt
-//                 players: {}    // IPs werden später eingetragen
-//             });
-//         }
-
-//         super.start();
-//     }
-
-//     // Spieler X/O zuweisen
-//     addPlayer(ip, name) {
-//         super.addPlayer(ip, name);
-
-//         const keys = Object.keys(this.players);
-//         const symbol = keys.length === 1 ? "X" : "O";
-
-//         set(ref(db, `games/${this.name}/players/${symbol}`), ip);
-//         set(ref(db, `games/${this.name}/board`), "000000000");
-//         set(ref(db, `games/${this.name}/turn`), "X");
-//     }
-
-//     initUI() {
-//         // this.gameUI.board = new ColorBlock("red", 80, 10);
-//         this.gameUI.board = new TextureBlock("Assets/bg3.png", 675, 10);
-//         this.gameUI.Can.slots = [
-//             this.gameUI.board.makeSlot({ x: 0, y: 0 }),
-//             ...Array.from({ length: 9 }, (_, i) => {
-//                 const pos = { x: -250 + (i % 3) * 250, y: -250 + Math.floor(i / 3) * 250 };
-//                 return [
-//                     this.gameUI.blocks[i].button.makeSlot(pos),
-//                     this.gameUI.blocks[i].image.makeSlot(pos)
-//                 ];
-//             }).flat()
-//         ];
-//         this.gameUI.Can.mount();
-//         this.gameUI.blocks.forEach((blockObj, index) => {
-//             blockObj.image.setVisibility(false);
-//             blockObj.button.addListener(async () => {
-//                 if (this.blocked) return;
-//                 this.blocked = true;
-//                 const ip = await getIP();
-//                 const snap = await get(ref(db, `games/${this.name}`));
-//                 const board = snap.val().board;
-//                 const players = snap.val().players;
-
-//                 let mySymbol = players.X === ip ? "X" : "O";
-//                 if (snap.val().turn !== mySymbol || board[index] !== "0") {this.blocked = false; return};
-//                 await set(ref(db, `games/${this.name}`), {
-//                     board: board.substring(0, index) + mySymbol + board.substring(index + 1),
-//                     turn: snap.val().turn === "X" ? "O" : "X",
-//                     players
-//                 });
-//                 this.blocked = false;
-//             });
-//         });
-
-//         onValue(ref(db, `games/${this.name}`), snapshot => {
-//             if (!snapshot.val() || !snapshot.val().board) return;
-//             const board = snapshot.val().board;
-//             for (let i = 0; i < 9; i++) {
-//                 const img = this.gameUI.blocks[i].image;
-//                 if (board[i] === "0") {
-//                     img.setVisibility(false);
-//                 } else {
-//                     img.setVisibility(true);
-//                     img.setImage(board[i] === "X" ? "Assets/Cross.png" : "Assets/Circle.png");
-//                 }
-//             }
-//         });
-//     }
-// }
     constructor() {
         super();
         this.id = "tik";
-    }
-
-    load(i) {
-        return super.load(i);
     }
 }
 
@@ -213,10 +92,6 @@ export class Sudoku extends game {
         super();
         this.id = "sudoku";
     }
-
-    load(i) {
-        return super.load(i);
-    }
 }
 
 export class Ship extends game {
@@ -224,19 +99,11 @@ export class Ship extends game {
         super();
         this.id = "ship";
     }
-
-    load(i) {
-        return super.load(i);
-    }
 }
 
 export class Find extends game {
     constructor() {
         super();
         this.id = "find";
-    }
-
-    load(i) {
-        return super.load(i);
     }
 }
--- gamelauncher/gamelauncher.js
+++ gamelauncher/gamelauncher.js
@@ -1,5 +1,7 @@
 import { games } from '../script/script.js';
 import { getIP } from '../utils.js';
+import { ref, onValue } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js";
+import { db } from '../firebasedata.js';
 
 console.log("gamelauncher start")
 const params = new URLSearchParams(location.search);
@@ -14,3 +16,20 @@ if (!GameClass) location.href = "/";
 const IP = await getIP();
 GameClass.addPlayer(IP);
 console.log("werw")
+
+const sessionRef = ref(db, `games/${gameName}/sessions`);
+
+onValue(sessionRef, snap => {
+    const sessions = snap.val();
+    if (!sessions) return;
+
+    for (const [sessionId, session] of Object.entries(sessions)) {
+        if (!session.started) continue;
+        if (!session.players) continue;
+
+        // Prüfen ob ICH in dieser Session bin
+        if (Object.values(session.players).includes(IP)) {
+            location.href = `${gameName}?session=${sessionId}`;
+        }
+    }
+});
